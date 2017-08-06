e32949f3fba405eb788169b3a8964c8df2993af7
Andrzej Lisaj <lisaj.and@gmail.com>
2017-08-06 20:33:04 +0200

    Listing cities on index.html

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
 
-app.get("/", function name(request, response) {
-   response.send('OK');
-});
+app.use(express.static('public'));
 
+app.get("/cities", function (request, response) {
+    var cities = ['Lotopia', 'Caspiana', 'Indigo'];
+    response.json(cities);
+});
 module.exports=app;
